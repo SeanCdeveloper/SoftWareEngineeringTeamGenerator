75c843af42f546d72a0e801115f9b21b4dddda3c
Your Name <you@example.com>
2020-02-06 00:53:44 -0500

    All tests passed.

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -3,7 +3,7 @@ const Manager = require("../lib/Manager");
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
+  expect(e._officeNumber).toBe(testValue);
 });
 
 test("getRole() should return \"Manager\"", () => {
